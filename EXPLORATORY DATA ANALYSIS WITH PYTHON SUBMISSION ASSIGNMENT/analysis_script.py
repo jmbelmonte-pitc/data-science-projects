74adb9e2b01f94011f967e744b15d8f99aea6bc5
jmbelmonte.pitc@gmail.com <jmbelmonte.pitc@gmail.com>
2026-06-23 00:18:20 +0800

    EXPLORATORY DATA ANALYSIS WITH PYTHON SUBMISSION ASSIGNMENT

---
+++ EXPLORATORY DATA ANALYSIS WITH PYTHON SUBMISSION ASSIGNMENT/analysis_script.py
@@ -0,0 +1,27 @@
+# For the univariate analysis, I chose the 'price' column because it is one of
+# the most important variables in the diamonds dataset. I used descriptive
+# statistics and a histogram to understand its distribution.
+
+# For the multivariate analysis, I selected 'carat' and 'price' because I wanted
+# to see if larger diamonds tend to have higher prices. I used a scatter plot
+# and correlation analysis to examine their relationship.
+
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+diamonds = sns.load_dataset('diamonds')
+
+print(diamonds.head())
+
+# Univariate Analysis
+print(diamonds['price'].describe())
+diamonds['price'].hist(bins=30)
+plt.show()
+
+# Multivariate Analysis
+sns.scatterplot(data=diamonds, x='carat', y='price')
+plt.show()
+
+print(diamonds[['carat','price']].corr())
